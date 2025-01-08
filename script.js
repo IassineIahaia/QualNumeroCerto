4766b25624b9a47223419974ae853dfe7e2f948c
Iassine Iahaia <zinhocabila58@gmail.com>
2025-01-08 15:58:35 +0200

    testando o javscript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+
+const resultado = document.querySelector('.message').textContent = 'Acertou!🎉';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
