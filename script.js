c65766797bb547aa1e53a21b4480d4750cfe3d91
Iassine Iahaia <zinhocabila58@gmail.com>
2025-01-08 16:28:25 +0200

    primeiros passos de js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 
-const resultado = document.querySelector('.message').textContent = 'Acertou!🎉';
+// const resultado = document.querySelector('.message').textContent = 'Acertou!🎉';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
+const number = document.querySelector('.number').textContent = 13;
+const score = document.querySelector('.score').textContent = 10;
+const guess = document.querySelector('.guess').value = 10;
+
+if(guess === number) {
+    document.querySelector('.message').textContent = 'Acertou!🎉';
+}else {
+    document.querySelector('.message').textContent = 'Errou! Tente novamente!';
+}
